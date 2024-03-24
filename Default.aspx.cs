8a07c37c2b65688f26dd69a2d992ccb3152c05c7
jannygames <jamolisdev@gmail.com>
2024-03-25 01:35:01 +0200

    Made a decent main page design

---
+++ Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlyZalgiris
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
